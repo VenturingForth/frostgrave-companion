6dd60f0daf1860b5ecf47c33d4df183b131e4c9c
Joshua Robinson <joshua@codeadventure.uk>
2024-04-05 12:59:44 +0100

    added general arms and armour test data

--- db/data/test-data/index.ts
+++ db/data/test-data/index.ts
@@ -1,2 +1,4 @@
 exports.wizardTypesData = require('./wizard_types.json');
 exports.soldiersData = require('./soldiers.json');
+exports.generalArmsData = require("./general_arms.json");
+exports.generalArmourData = require("./general_armour.json");
