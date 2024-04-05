fd0690c410df34b57199fc075a8e65f2bbeb5f67
Joshua Robinson <joshua@codeadventure.uk>
2024-04-05 13:19:00 +0100

    added magic items test data

--- db/data/test-data/index.ts
+++ db/data/test-data/index.ts
@@ -1,6 +1,7 @@
-exports.wizardTypesData = require('./wizard_types.json');
-exports.soldiersData = require('./soldiers.json');
+exports.wizardTypesData = require("./wizard_types.json");
+exports.soldiersData = require("./soldiers.json");
 exports.generalArmsData = require("./general_arms.json");
 exports.generalArmourData = require("./general_armour.json");
 exports.potionsData = require("./potions.json");
-exports.magicWeaponsAndArmourData = require('./magic_weapons_and_armour.json');
+exports.magicWeaponsAndArmourData = require("./magic_weapons_and_armour.json");
+exports.magicItemsData = require("./magic_items.json");
