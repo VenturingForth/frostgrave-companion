f205b80a851f1eeb537e5fd2e9dd34bea27b287e
Joshua Robinson <joshua@codeadventure.uk>
2024-04-05 13:04:38 +0100

    added potions test data

--- db/data/test-data/index.ts
+++ db/data/test-data/index.ts
@@ -2,3 +2,4 @@ exports.wizardTypesData = require('./wizard_types.json');
 exports.soldiersData = require('./soldiers.json');
 exports.generalArmsData = require("./general_arms.json");
 exports.generalArmourData = require("./general_armour.json");
+exports.potionsData = require("./potions.json");
